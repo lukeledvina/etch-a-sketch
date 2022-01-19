c692fa8fa312ef06735e190b7922107e4ed92142
luke <lukeledvina19@gmail.com>
2022-01-19 15:40:08 -0500

    Add event listener to check for mouse over on boxes to turn them black

--- script.js
+++ script.js
@@ -7,7 +7,16 @@ for(let i = 0; i < 16; i++) {
     subContainer.setAttribute('class', 'sub-container');
     for(let i = 0; i < 16; i++) {
         let childDiv = document.createElement('div');
+        childDiv.setAttribute('class', 'box');
         subContainer.appendChild(childDiv);
+
     }
     container.appendChild(subContainer);
 }
+
+const boxes = document.querySelectorAll(".box");
+
+boxes.forEach(box => {
+
+    box.addEventListener("mouseover", () => box.style.backgroundColor = "black");
+});
